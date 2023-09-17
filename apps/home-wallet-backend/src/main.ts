c4436b47b456312b13df146d6e5fad2c662fe5bf
Konrad Slomiany <k.slomiany@exlabs.pl>
2023-09-17 11:59:58 +0200

    last try

--- apps/home-wallet-backend/src/main.ts
+++ apps/home-wallet-backend/src/main.ts
@@ -8,8 +8,12 @@ app.get('/users', async (req, res) => {
   res.json("Hello World")
 })
 
+app.get('/test', async (req, res) => {
+  res.json("Hello test")
+})
+
 
 
 app.listen(10000, () =>
-  console.log('REST API server ready at: http://localhost:3000'),
+  console.log('REST API server ready at: http://localhost:10000'),
 )
