a9cceef8436e9968002c01bd9164d4e313056aad
Konrad Slomiany <k.slomiany@exlabs.pl>
2023-09-17 11:45:46 +0200

    next

--- apps/home-wallet-backend/src/main.ts
+++ apps/home-wallet-backend/src/main.ts
@@ -1,27 +1,14 @@
-import { PrismaClient } from '@prisma/client'
 import express from 'express'
 
-const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
 
 app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
+  res.json("Hello World")
 })
 
-app.post(`/user`, async (req, res) => {
-  try {
-    const result = await prisma.user.create({
-      data: { ...req.body },
-    })
-    res.json(result)
-  } catch (e) {
-    console.error(e)
-  }
- 
-})
+
 
 app.listen(3000, () =>
   console.log('REST API server ready at: http://localhost:3000'),
